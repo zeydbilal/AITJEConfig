81e3317cf3c80a391ac493a1577c8c332c2e5820
Werner Lamprecht <werner.lamprecht.fl@ait.ac.at>
2015-11-12 21:16:31 +0100

    final Sensor.class with documentation
    TODO  resize the error label

--- src/main/java/org/jevis/structurecreator/Sensor.java
+++ src/main/java/org/jevis/structurecreator/Sensor.java
@@ -1,55 +1,65 @@
 /**
- * Copyright (C) 2015 Werner Lamprecht
+ * Copyright (C) 2015 WernerLamprecht <werner.lamprecht@ymail.com>
  *
- * This file is part of JEVisExample.
- *
- * JEVisExample is free software: you can redistribute it and/or modify it under
+ * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation in version 3.
  *
- * JEVisExample is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * JEVisExample. If not, see <http://www.gnu.org/licenses/>.
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * JEVisExample is part of the OpenJEVis project, further project information
- * are published at <http://www.OpenJEVis.org/>.
+ * This class is part of the OpenJEVis project, further project information are
+ * published at <http://www.OpenJEVis.org/>.
  */
 package org.jevis.structurecreator;
 
 /**
- *
- * @author Werner Lamprecht <werner.lamprecht@ymail.com>
+ * This class is used for collecting Wiotech sensor details
  */
 public class Sensor {
 
     private String name;
     //private String prefix;
+    /**
+     * Human readable symbol
+     * °C - Temp
+     * ‰ - CO2
+     * % - rH
+     */
     private String symbol;
     private String unit;
     private String table;
     private String mac;
 
-    
-    public Sensor(String wiotechSensorableName) {
+    /**
+     * 
+     * @param wiotechSensorableName The Sensor name with 'sensor_' as prefix
+     * the mac address as name and the Wiotech unit number like
+     * sensor_1234567890ABCDEF_1
+     */
+    public Sensor(String wiotechSensorTableName) {
         
-        String[] sensorDetails = wiotechSensorableName.split("_");
+        String[] sensorDetails = wiotechSensorTableName.split("_");
         this.unit = convertAndSetUnit(sensorDetails[2]);
         this.mac = sensorDetails[1];
-        this.table = wiotechSensorableName;
+        this.table = wiotechSensorTableName;
         this.name = mac;
         
     }
-   
+   /**
+    * 
+    * @return The Sensor name with 'sensor_' as prefix
+    * the mac address as name and the Wiotech unit number like
+    * sensor_1234567890ABCDEF_1
+    */
     public String getTable(){
         return table;
     }
     
-
-
     public String getName() {
         return name;
     }
@@ -66,30 +76,46 @@ public class Sensor {
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }*/
-
+    /**
+     * 
+     * @return human readable symbol
+     */
     public String getSymbol() {
         return symbol;
     }
-
-    public void setSymbol(String unitSymbol) {
+    
+    /**
+     * 
+     * @param unitSymbol human readable symbol
+     */
+    private void setSymbol(String unitSymbol) {
         this.symbol = symbol;
     }
-
+    
+    /**
+     * 
+     * @return Unicode unit like "\u00b0C" for °C
+     */
     public String getUnit() {
         return unit;
     }
-
+    
+    /**
+     * Converts the Wiotech unit number into a unicode string
+     * @param wiotechUnitNumber
+     * @return Unicode unit like "\u00b0C" for °C
+     */
     private String convertAndSetUnit(String wiotechUnitNumber){
         switch(wiotechUnitNumber){
             case "1": //°C
                 this.symbol="Temp";
                 this.unit = "\u00b0C";
                 return this.getUnit();
-            case "6"://%
+            case "6"://‰
                 this.symbol="CO2";
                 this.unit = "\u0025";
                 return  this.getUnit();
-            case "9":// ‰
+            case "9":// %
                 this.symbol="rH";
                 this.unit = "\u0025";
                 return this.getUnit();    
@@ -97,11 +123,10 @@ public class Sensor {
             default:
                 return null;
         }
-        
     }
 
     /**
-     * @return the mac
+     * @return the sensor's mac
      */
     public String getMac() {
         return mac;
